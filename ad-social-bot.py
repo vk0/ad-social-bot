7c87cdcc50fe9575be0198a36e17ac4e2205f3ad
One1L <one1l@yandex.ru>
2015-12-14 15:14:10 +0700

    Groups

--- ad-social-bot.py
+++ ad-social-bot.py
@@ -3,9 +3,30 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import random
 from pyvirtualdisplay import Display
+from selenium.webdriver.common.action_chains import ActionChains
 
-display = Display(visible=1, size=(800, 600))
-display.start()
+def remove_last_group():
+    """driver.find_element_by_tag_name('body').send_keys(Keys.ALT + '2')
+    groups = driver.find_elements_by_css_selector('#groups_list_groups .group_row_labeled > a')
+    sleep(3)
+    print(len(groups))
+    if len(groups) > 1:
+        groups[-1].click()
+        #driver.find_element_by_id('group_actions_wrap').click()
+        #sleep(1)
+        #group_actions = driver.find_element_by_class_name('group_actions_item')
+        #group_actions[-1].click()
+    driver.find_element_by_tag_name('body').send_keys(Keys.ALT + '1')"""
+    groups_action_wrap = driver.find_element_by_id('group_actions_wrap')
+    ActionChains(driver).move_to_element(groups_action_wrap)
+    groups_action_wrap.click()
+    sleep(1)
+    group_actions = driver.find_element_by_class_name('group_actions_item')
+    group_actions[-1].click()
+
+
+#display = Display(visible=1, size=(800, 600))
+#display.start()
 
 driver = webdriver.Firefox()
 
@@ -23,6 +44,12 @@ sleep(2)
 
 print('Logged')
 
+main_window = driver.current_window_handle
+
+#driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+#driver.get('http://vk.com/groups')
+#driver.find_element_by_tag_name('body').send_keys(Keys.ALT + '1')
+
 print('Loggin to ad social')
 
 driver.get('http://ad-social.org/')
@@ -35,11 +62,13 @@ sleep(3)
 
 print('Logged')
 
+sleep(3)
+
 while True:
-    driver.get('http://ad-social.org/vk/earn?type=like')
+    driver.get('http://ad-social.org/vk/earn?soc=vk&type=group')
     sleep(3)
     perform_buttons = driver.find_elements_by_css_selector('.action-bar > .openTask.btn')
-    main_window = driver.current_window_handle
+
     for button in perform_buttons:
         button.click()
         sleep(3)
@@ -48,16 +77,25 @@ while True:
         if not window_handles:
             continue
         driver.switch_to.window(window_handles[0])
+        print(driver.current_url)
 
-        action_buttons = driver.find_elements_by_css_selector('.fw_like_link.fl_l') or\
+        action_buttons = driver.find_elements_by_id('subscribe_button') or\
+                        driver.find_elements_by_css_selector('#group_like_module > .flat_button.button_big.button_wide') or\
+                        driver.find_elements_by_css_selector('.fw_like_link.fl_l') or\
                         driver.find_elements_by_id('pv_like_wrap')
         if action_buttons:
             action_buttons[0].click()
+            driver.switch_to.window(main_window)
             sleep(random.randrange(5, 7, 1))
         sleep(2)
-        driver.close()
+        driver.switch_to.window(window_handles[0])
         sleep(1)
+        remove_last_group()
+        sleep(1)
+        driver.close()
         driver.switch_to.window(main_window)
+        sleep(1)
+
         print(driver.find_element_by_css_selector('a[href="/profile/balance"] > span').text)
 
 #exit_button = driver.find_elements_by_css_selector('a[href="http://ad-social.org/dashboard/logout"]')[0]
